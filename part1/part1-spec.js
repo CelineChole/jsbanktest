62260d40d921ea455291858d10045f5444241e5d
Tim Addison <git@tjaddison.com>
2018-12-23 17:23:05 -0500

    Update

--- part1/part1-spec.js
+++ part1/part1-spec.js
@@ -26,11 +26,45 @@ describe('The Bank - Part 1', () => {
     it('Has some password security', () => {
         const customer = new Customer("John Smith", 1, "secret");
         // We can't access the customers password after it is created
+        // Note this is NOT robust security!
         expect(customer.Secret).toBe(undefined);
 
         // Instead we can check if the password matches
         expect(customer.IsPassword("secret").toBe(true));
         
         expect(customer.IsPassword("nothispassword").toBe(false));
+    });
+
+    it('Allows us to get a customer from the bank with an Id', () => {
+        const bank = new Bank();
+
+        const john = bank.CreateCustomer("John Smith", "secret");
+        const samara = bank.CreateCustomer("Samara Dacre", "potato");
+        
+        expect(john.Id).toBe(1);
+        expect(samara.Id).toBe(2);
+        expect(john.IsPassword("secret")).toBe(true);
+        expect(samara.IsPassword("potato")).toBe(true);
+    });
+
+    it('Allows us to find a customer by Id', () => {
+        const bank = new Bank();
+
+        bank.CreateCustomer("John Smith", "secret");
+        bank.CreateCustomer("Samara Dacre", "potato");
+
+        // Returns the customer object
+        expect(bank.FindCustomerById(1).Name).toBe("John Smith");
+        expect(bank.FindCustomerById(2).IsPassword("potato")).toBe(true);
+    });
+
+    it('Does not return the customer objects we put it', () => {
+        const bank = new Bank();
+
+        const john = bank.CreateCustomer("John Smith", "secret");
+
+        // Modifying this object shouldn't modify the bank's data
+        john.Name = "Hacker Stan";
+        expect(bank.FindCustomerById(1).Name).toBe("John Smith");
     })
 });
